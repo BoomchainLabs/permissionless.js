f7171da611693ea829fea2249f35fd6ad7d16c4d
Tewodros Mitiku <tedi@privy.io>
2026-05-26 17:57:16 -0400

    export

---
+++ packages/permissionless/accounts/kernel/utils/index.ts
@@ -0,0 +1,7 @@
+export {
+  wrapMessageHash,
+  type WrapMessageHashParams,
+} from "./wrapMessageHash.js";
+export { signMessage } from "./signMessage.js";
+export { signTypedData } from "./signTypedData.js";
+export { isKernelV2 } from "./isKernelV2.js";
